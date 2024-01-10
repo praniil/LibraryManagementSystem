a6b3d211c60faa8965b88b095fd8bf90496d3c34
praniil <pparajuli15@gmail.com>
2024-01-10 08:24:06 +0545

    feat: added a method to get a book from the database

--- controllers/bookController.go
+++ controllers/bookController.go
@@ -2,11 +2,16 @@ package controllers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"libraryManagementSystem/database"
 	"libraryManagementSystem/models"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 )
 
 type Response struct {
@@ -47,5 +52,47 @@ func insertBook(book models.Book) int64 {
 	}
 	fmt.Printf("Inserted a single record %v \n", book.ID)
 	return int64(book.ID)
+
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+
+	//get the book id from request params
+	params := mux.Vars(r)
+	// /api/getbook/{id}
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		log.Fatalf("unable to convert the string to int %v", err)
+	}
+
+	book, err := getBook(int64(id))
+	if err != nil {
+		fmt.Println("unable to get the book")
+	}
+
+	json.NewEncoder(w).Encode(book)
+
+}
+
+func getBook(id int64) (models.Book, error) {
+	db := database.Database_connection()
+
+	var book models.Book
+	result := db.First(&book, id) //db.first returns first record that matches the condition and returns to &book
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		fmt.Println("no rows were returned")
+		return book, nil
+	} else if result.Error != nil {
+		log.Fatalf("unable to query the book. %v", result.Error)
+		return book, result.Error
+	}
+	fmt.Println("book in original format", book)
+	return book, nil
+
+	//original format from database {1 University Physics 2000 Conceptual Physics 1982-02-16}
+	//later on encoded to json format while giving response to the user
 
 }
--- router/router.go
+++ router/router.go
@@ -1,12 +1,13 @@
 package router
 
-import(
-	"libraryManagementSystem/controllers"
+import (
 	"github.com/gorilla/mux"
+	"libraryManagementSystem/controllers"
 )
 
 func Router() *mux.Router {
-	router:= mux.NewRouter();
+	router := mux.NewRouter()
 	router.HandleFunc("/api/books/newbook", controllers.CreateBook).Methods("POST", "OPTIONS")
+	router.HandleFunc("/api/books/getbook/{id}", controllers.GetBook).Methods("GET", "OPTIONS")
 	return router
 }
