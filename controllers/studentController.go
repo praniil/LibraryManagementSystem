2daf5f7f3170883875eb548a00fc56dd8c9fa6b7
praniil <pparajuli15@gmail.com>
2024-01-15 11:15:30 +0545

    feat: added methods to update the rows of student according to the mentioned id in the api

--- controllers/studentController.go
+++ controllers/studentController.go
@@ -2,11 +2,16 @@ package controllers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"libraryManagementSystem/database"
 	"libraryManagementSystem/models"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 )
 
 func CreateStudent(w http.ResponseWriter, r *http.Request) {
@@ -40,4 +45,40 @@ func insertStudent(student models.Student) int64 {
 	return int64(student.ID)
 }
 
-func UpdateStudent(w http.)
+func UpdateStudent(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "PUT")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"]) //{id} extract
+	if err != nil {
+		log.Fatalf("couldnot extract id from the url, %v", err)
+	}
+	var student models.Student
+	json.NewDecoder(r.Body).Decode(&student)
+
+	rowsUpdated := updateStudent(student, int64(id))
+	msg := fmt.Sprintf("number of rows updated: %d", rowsUpdated)
+	res := Response{
+		ID:      int64(id),
+		Message: msg,
+	}
+	json.NewEncoder(w).Encode(res)
+
+}
+
+func updateStudent(student models.Student, id int64) int64 {
+	db := database.Database_connection()
+	result := db.Model(&models.Student{}).Where("id = ?", id).Updates(student)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		fmt.Printf("Record not found with id: %d", id)
+	}
+	if result.Error != nil {
+		log.Fatalf("unable to update the record . %v", result.Error)
+	}
+	rowsUpdated := result.RowsAffected
+	return rowsUpdated
+
+}
